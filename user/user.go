bf6f695e8a2cd37b59d0c345f64422de7314c928
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-12-18 12:30:24 +0530

    a REST API using Fiber and Gorm

--- user/user.go
+++ user/user.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 var err error
 
-const DNS = "your data base connection"
+const DNS = "your database url"
 
 type User struct {
 	gorm.Model
@@ -31,21 +31,52 @@ func InitialMigration() {
 }
 
 func GetUsers(c *fiber.Ctx) error {
-
+	var users []User
+	DB.Find(&users)
+	return c.JSON(&users)
 }
 
 func GetUser(c *fiber.Ctx) error {
-	
+	id := c.Params("id")
+	var user User
+	DB.Find(&user, id)
+	return c.JSON(&user)
 }
 
 func SaveUser(c *fiber.Ctx) error {
-	
+	user := new(User)
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	DB.Create(&user)
+	return c.JSON(&user)
 }
 
 func DeleteUser(c *fiber.Ctx) error {
-	
+	id := c.Params("id")
+	var user User
+	DB.First(&user, id)
+	if user.Email == "" {
+		return c.Status(500).SendString("User not availabe")
+	}
+
+	DB.Delete(&user)
+	return c.SendString("User is deleted")
 }
 
 func UpdateUser(c *fiber.Ctx) error {
-	
+	id := c.Params("id")
+	user := new(User)
+	DB.First(&user, id)
+	if user.Email == "" {
+		return c.Status(500).SendString("User not available")
+	}
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	DB.Save(&user)
+	return c.JSON(&user)
 }
